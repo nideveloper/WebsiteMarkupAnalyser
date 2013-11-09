a786fbc5c202599292e45aba78c08c6f0f85cc77
Matt <matt@vs-software.co.uk>
2013-11-09 16:19:01 +0000

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,118 @@
+function addURLPrefix(url){
+    if (url == '') {
+        url += '?';
+    } else {
+        url += '&';
+    }
+
+    return url;
+}
+
+chrome.browserAction.onClicked.addListener(function () {
+    chrome.tabs.executeScript(
+    {
+        code: "document.getElementsByTagName('html')[0].innerHTML;"
+    },
+    function (ps1) {
+        var html = $.parseHTML('<html>' + ps1.toString() + '</html>');
+        var inputs = $(html).find('input, select, textarea');
+        var tables = $(html).find('table');
+        var title = $(html).filter('title').text();
+        var inputsWithoutID = [];
+        var inputsWithoutName = [];
+        var inputsWithoutLabel = [];
+        var radiosWithoutValidationLabel = [];
+        var numInlineStyles = 0;
+        var numTables = tables.length;
+        var numInputs = inputs.length;
+        var url = '';
+        var safeObject;
+        var safeNameObject;
+        var safeLabelObject;
+        var safeRadioObject;
+
+        $(inputs).each(function () {
+            if (($(this).attr('type') !== 'submit') && ($(this).attr('type') !== 'button')) {
+                if ($(this).attr('id') == undefined) {
+                    inputsWithoutID.push($(this).attr('name') + ' - ' + $(this).get(0).tagName + ' - ' + $(this).attr('type'));
+                }
+                else {
+                    if ($(this).attr('type') !== 'hidden') {
+                        var label = $('label[for="' + $(this).attr('id') + '"]');
+                        if (label.length == 0) {
+                            inputsWithoutLabel.push($(this).attr('id') + ' - ' + $(this).get(0).tagName + ' - ' + $(this).attr('type'));
+                        }
+                    }
+                }
+                if ($(this).attr('name') == undefined) {
+                    inputsWithoutName.push($(this).attr('id') + ' - ' + $(this).get(0).tagName + ' - ' + $(this).attr('type'));
+                }
+
+                if ($(this).attr('type') === 'radio') {
+                    if ($(this).attr('data-validationLabel') == undefined) {
+                        radiosWithoutValidationLabel.push($(this).attr('name'));
+                    }
+                }
+            }
+        });
+
+        $(html).find('*').each(function () {
+            if ($(this).attr('style') != undefined) {
+                numInlineStyles++;
+            }
+        });
+
+        safeObject = encodeURI(inputsWithoutID);
+        safeNameObject = encodeURI(inputsWithoutName);
+        safeLabelObject = encodeURI(inputsWithoutLabel);
+        safeRadioObject = encodeURI(radiosWithoutValidationLabel);
+
+        if (safeObject!='') {
+            url = addURLPrefix(url);
+            url += 'elementNames=' + safeObject;
+        }
+
+        if (safeNameObject.length != '') {
+            url = addURLPrefix(url);
+            url += 'elementIDs=' + safeNameObject;
+        }
+
+        if (safeLabelObject != '') {
+            url = addURLPrefix(url);
+            url += 'elementsWithoutLabel=' + safeLabelObject;
+        }
+
+        if (safeRadioObject != '') {
+            url = addURLPrefix(url);
+            url += 'radiosWithoutValidationLabel=' + safeRadioObject;
+        }
+
+        if (numInlineStyles > 0) {
+            url = addURLPrefix(url);
+            url += 'numInlineStyles=' + numInlineStyles;
+        }
+
+        if (numTables > 0) {
+            url = addURLPrefix(url);
+            url += 'numTables=' + numTables;
+        }
+
+        if (numTables > 0) {
+            url = addURLPrefix(url);
+            url += 'numInputs=' + numInputs;
+        }
+
+        if(title != ''){
+            url = addURLPrefix(url);
+            url += 'title=' + title;
+        }
+
+        chrome.tabs.create({ 'url': 'tab.html' + url }, function (tab) {
+            // Tab opened.
+        });
+
+    }
+  );
+});
+
+
---
+++ js/screenStatistics.js
@@ -0,0 +1,93 @@
+function getURLParameter(name) {
+    return decodeURIComponent(
+        (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
+    );
+}
+
+jQuery(document).ready(function () {
+    var elementNamesString = getURLParameter('elementNames');
+    if ('null' != elementNamesString) {
+        var elementNamesArray = elementNamesString.split(',');
+        var convertedString = '<table class="dataTable table table-striped"><thead><tr><th>Name - Element - Type</th></tr></thead>';
+        $(elementNamesArray).each(function () {
+            if (this != '') {
+                convertedString += '<tr><td>';
+                convertedString += this;
+                convertedString += '</td></tr>';
+            }
+        });
+        convertedString += '</table>';
+        $('#missingIDContainer').html(convertedString);
+    }
+
+    var elementIDsString = getURLParameter('elementIDs');
+    if ('null' != elementIDsString) {
+        var elementIDArray = elementIDsString.split(',');
+        var convertedString2 = '<table class="dataTable table table-striped"><thead><tr><th>ID - Element - Type</th></tr></thead>';
+        $(elementIDArray).each(function () {
+            if (this != '') {
+                convertedString2 += '<tr><td>';
+                convertedString2 += this;
+                convertedString2 += '</td></tr>';
+            }
+        });
+        convertedString2 += '</table>';
+        $('#missingNameContainer').html(convertedString2);
+    }
+
+    var elementWithoutLabelString = getURLParameter('elementsWithoutLabel');
+    if ('null' != elementWithoutLabelString) {
+        var elementsWithoutLabelArray = elementWithoutLabelString.split(',');
+        var convertedString3 = '<table class="dataTable table table-striped"><thead><tr><th>ID - Element - Type</th></tr></thead>';
+        $(elementsWithoutLabelArray).each(function () {
+            if (this != '') {
+                convertedString3 += '<tr><td>';
+                convertedString3 += this;
+                convertedString3 += '</td></tr>';
+            }
+        });
+        convertedString3 += '</table>';
+        $('#withIDMissingLabelContainer').html(convertedString3);
+    }
+
+    var radiosWithoutValidationLabelString = getURLParameter('radiosWithoutValidationLabel');
+    if ('null' != radiosWithoutValidationLabelString) {
+        var radiosWithoutValidationLabelArray = radiosWithoutValidationLabelString.split(',');
+        var convertedString3 = '<table class="dataTable table table-striped"><thead><tr><th>Name</th></tr></thead>';
+        $(radiosWithoutValidationLabelArray).each(function () {
+            if (this != '') {
+                convertedString3 += '<tr><td>';
+                convertedString3 += this;
+                convertedString3 += '</td></tr>';
+            }
+        });
+        convertedString3 += '</table>';
+        $('#radiosWithoutValidationLabelContainer').html(convertedString3);
+    }
+
+
+
+    var numInlineStyles = getURLParameter('numInlineStyles');
+    if ('null' != numInlineStyles) {
+        $('#propertyTableBody').append('<tr><td>Number of Inline Styles</td><td>' + numInlineStyles + '</td></tr>');
+    }
+
+    var numTables = getURLParameter('numTables');
+    if ('null' != numTables) {
+        $('#propertyTableBody').append('<tr><td>Number of Tables on the page</td><td>' + numTables + '</td></tr>');
+    }
+
+    var numInputs = getURLParameter('numInputs');
+    if ('null' != numInputs) {
+        $('#propertyTableBody').append('<tr><td>Number of Inputs (Input, Select, Textarea) on the page</td><td>' + numInputs + '</td></tr>');
+    }
+    var pageTitle = getURLParameter('title');
+    if ('null' != pageTitle) {
+        $('#pageTitle').append(pageTitle);
+    }
+
+    $('.dataTable').dataTable({
+        "aLengthMenu": [[5, 10, 15, 25, 50, 100, -1], [5, 10, 15, 25, 50, 100, "All"]],
+        "iDisplayLength": 5
+    });
+});
